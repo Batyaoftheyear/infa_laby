319c816a5c4a5152a3bbab5ea56093985aa324a2
Anton Bobotov <98155212+Batyaoftheyear@users.noreply.github.com>
2022-01-23 22:05:26 +0300

    Add files via upload

---
+++ arithmetic_dragons/gameunit.py
@@ -0,0 +1,14 @@
+# coding: utf-8
+# license: GPLv3
+
+
+class Attacker:
+    _health = 0
+    _attack = 0
+
+    def attack(self, target):
+        target._health -= self._attack
+
+    def is_alive(self):
+        return self._health > 0
+    
